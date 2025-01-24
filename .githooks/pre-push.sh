6ae302df5b3ae6206bea50bd7e64a831c3cba687
necusjz <necusjz@gmail.com>
2025-01-24 15:40:56 +1100

    feat: add pre-push scripts

---
+++ .githooks/pre-push.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# define color codes
+GREEN="\033[0;32m"
+YELLOW="\033[0;33m"
+NC="\033[0m"  # no color
+
+# Get the list of changed files
+ChangedFiles=$(git status --porcelain | sed -E 's/^\s*\S+\s+//')
+
+# Extract modified modules
+Modules=$(echo "$ChangedFiles" | 
+    grep "^Commands/" |  # Filter files under commands folder
+    sed -E 's/^Commands\///' |  # Remove the prefix
+    cut -d '/' -f 1 |  # Extract the module
+    sort -u)  # Ensure uniqueness
+
+echo -e "${GREEN}Verifying command models...${NC}"
+
+# Get Git root directory
+GitRoot=$(git rev-parse --show-toplevel)
+
+# Iterate through the modules
+for Module in $Modules; do
+    echo -e "${YELLOW}Running: aaz-dev command-model verify -a $GitRoot -t $Module${NC}" # For logging
+    aaz-dev command-model verify -a "$GitRoot" -t "$Module"
+    if [[ $? -ne 0 ]]; then
+        exit 1
+    fi
+done
+
+echo -e "\n${GREEN}All models are consistent.${NC}"
+
+exit 0
